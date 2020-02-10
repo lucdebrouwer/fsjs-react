a1a51a3d22bcad093a69c2999f1df3ce0f25d8f1
lucdebrouwer <lljdebrouwer@gmail.com>
2020-02-11 00:02:02 +0100

    added html for courseDetail, createCourse, updateCourse

--- src/components/courses/CourseDetail.js
+++ src/components/courses/CourseDetail.js
@@ -1,15 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class CourseDetail extends Component {
-    render() {
-        return (
-            <div>
-                
+  render() {
+    return (
+      <div className="actions--bar">
+        <div className="bounds">
+          <div className="grid-100">
+            <span>
+              <a className="button" href="update-course.html">
+                Update Course
+              </a>
+              <a className="button" href="#">
+                Delete Course
+              </a>
+            </span>
+            <a className="button button-secondary" href="index.html">
+              Return to List
+            </a>
+          </div>
+        </div>
+        <div class="bounds course--detail">
+          <div className="grid-66">
+            <div className="course--header">
+              <h4 className="course--label">Course</h4>
+              <h3 className="course--title">Build a Basic Bookcase</h3>
+              <p>By Joe Smith</p>
             </div>
-        )
-    }
-    
+            <div className="course--description">
+              <p>My course description</p>
+            </div>
+          </div>
+          <div className="grid-25 grid-right">
+            <div className="course--stats">
+              <ul className="course--stats--list">
+                <li className="course--stats--list--item">
+                  <h4>Estimated Time</h4>
+                  <h3>14 hours</h3>
+                </li>
+                <li className="course--stats--list--item">
+                  <h4>Materials Needed</h4>
+                  <ul>
+                    <li>some material</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default CourseDetail;
--- src/components/courses/CreateCourse.js
+++ src/components/courses/CreateCourse.js
@@ -1,13 +1,92 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class CreateCourse extends Component {
-    render() {
-        return (
-            <div>
-                
+  render() {
+    return (
+      <div className="bounds course--detail">
+        <h1>Create Course</h1>
+        <div>
+          <div>
+            <h2 className="validation--errors--label">Validtion errors</h2>
+            <div className="validation-errors">
+              <ul>
+                <li>Test</li>
+              </ul>
             </div>
-        )
-    }
+            <form>
+              <div className="grid-66">
+                <div className="course--header">
+                  <h4 className="course--label">Course</h4>
+                  <div>
+                    <input
+                      id="title"
+                      name="title"
+                      type="text"
+                      className="input-title course--title--input"
+                      placeholder="Course title..."
+                      value=""
+                    />
+                  </div>
+                  <p>By Test</p>
+                </div>
+                <div className="course--description">
+                  <div>
+                    <textarea
+                      id="description"
+                      name="description"
+                      className=""
+                      placeholder="Course description..."
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="grid-25 grid-right">
+                  <div className="course--stats">
+                    <ul className="course--stats--list">
+                      <li className="course--stats--list--item">
+                        <h4>Estimated Time</h4>
+                        <div>
+                          <input
+                            id="estimatedTime"
+                            name="estimatedTime"
+                            type="text"
+                            className="course--time--input"
+                            placeholder="Hours"
+                            value=""
+                          />
+                        </div>
+                      </li>
+                      <li className="course--stats--list--item">
+                        <h4>Materials Needed</h4>
+                        <div>
+                          <textarea
+                            id="materialsNeeded"
+                            name="materialsNeeded"
+                            className=""
+                            placeholder="List materials..."
+                          ></textarea>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="grid-100 pad-bottom">
+                <button class="button" type="submit">
+                  Create Course
+                </button>
+                <button
+                  className="button button-secondary"
+                  onclick="event.preventDefault(); location.href='index.html';"
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CreateCourse;
